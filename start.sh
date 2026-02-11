77b6e4765a0f95694006e3358244448aa7dc08a0
VoidXDev <developershiva18@gmail.com>
2026-02-11 20:19:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+# Bot start karna (background mein)
+python3 -m bot.main &
+
+# Dummy Flask server (Render port check pass karne ke liye)
+# Render default PORT environment variable deta hai
+python3 -c "from flask import Flask; app = Flask(__name__); @app.route('/')\
+def health(): return 'Bot is Running'; app.run(host='0.0.0.0', port=$PORT)"
